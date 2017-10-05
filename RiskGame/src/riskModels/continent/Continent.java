56c1e9e68a4e07864aa6bf0359da965e25c5f893
prashantp995 <prashantp95@gmail.com>
2017-10-05 10:30:41 -0400

    [Update] Added numberofTerritories attribute.

--- RiskGame/src/riskModels/continent/Continent.java
+++ RiskGame/src/riskModels/continent/Continent.java
@@ -4,22 +4,39 @@ import java.util.ArrayList;
 import java.util.List;
 
 import riskModels.country.Country;
-
+/**
+ * Continent Bean class that will hold the properties of continent.
+ * @author prashantp95
+ *
+ */
 public class Continent {
 	
 	public String continentName;
 	public List<Country> memberCountriesList = new ArrayList<Country>();
-	public String getContinentName() {
+	public int nummberOfTerritories;
+	
+	public String getContinentName() 
+	{
 		return continentName;
 	}
-	public void setContinentName(String continentName) {
+	public void setContinentName(String continentName) 
+	{
 		this.continentName = continentName;
 	}
-	public List<Country> getMemberCountriesList() {
+	public List<Country> getMemberCountriesList()
+	{
 		return memberCountriesList;
 	}
-	public void setMemberCountriesList(List<Country> memberCountriesList) {
+	public void setMemberCountriesList(List<Country> memberCountriesList)
+	{
 		this.memberCountriesList = memberCountriesList;
 	}
+	public int getNummberOfTerritories() {
+		return nummberOfTerritories;
+	}
+	public void setNummberOfTerritories(int nummberOfTerritories) {
+		this.nummberOfTerritories = nummberOfTerritories;
+	}
+	
 	
 }
