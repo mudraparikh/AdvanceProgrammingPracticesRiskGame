2ff0ac37f1ed10b24c52022a4f48e3f87bfc2708
prashantp995 <prashantp95@gmail.com>
2017-10-05 09:02:21 -0400

    [Update] Updated the constants file and readCountry Method

--- RiskGame/src/riskModels/country/Country.java
+++ RiskGame/src/riskModels/country/Country.java
@@ -3,6 +3,11 @@ package riskModels.country;
 import java.util.HashMap;
 import java.util.List;
 
+/**
+ * Country Bean class to get and set properties related to country
+ * @author prashantp95
+ *
+ */
 
 public class Country {
 	public String countryName;
@@ -11,34 +16,54 @@ public class Country {
 	public String belongsToContinet;// to Represent county belongs to which continent 
 	public List<Country> neighborNodes; // to Represents adjacent country nodes
 	
-	public String getCountryName() {
+	public Country(String countryName, int startPixel, int endPixel, String continentName)
+	{
+		this.countryName=continentName;
+		this.startPixel= startPixel;
+		this.endPixel=endPixel;
+		this.belongsToContinet=continentName;
+	}
+	public Country(String neighbourCountryName)
+	{
+		this.countryName = neighbourCountryName;
+	}
+	public String getCountryName() 
+	{
 		return countryName;
 	}
-	public void setCountryName(String countryName) {
+	public void setCountryName(String countryName)
+	{
 		this.countryName = countryName;
 	}
 	public int getStartPixel() {
 		return startPixel;
 	}
-	public void setStartPixel(int startPixel) {
+	public void setStartPixel(int startPixel)
+	{
 		this.startPixel = startPixel;
 	}
-	public int getEndPixel() {
+	public int getEndPixel()
+	{
 		return endPixel;
 	}
-	public void setEndPixel(int endPixel) {
+	public void setEndPixel(int endPixel)
+	{
 		this.endPixel = endPixel;
 	}
-	public String getBelongsToContinet() {
+	public String getBelongsToContinet()
+	{
 		return belongsToContinet;
 	}
-	public void setBelongsToContinet(String belongsToContinet) {
+	public void setBelongsToContinet(String belongsToContinet)
+	{
 		this.belongsToContinet = belongsToContinet;
 	}
-	public List<Country> getNeighborNodes() {
+	public List<Country> getNeighborNodes()
+	{
 		return neighborNodes;
 	}
-	public void setNeighborNodes(List<Country> neighborNodes) {
+	public void setNeighborNodes(List<Country> neighborNodes) 
+	{
 		this.neighborNodes = neighborNodes;
 	}
 	
--- RiskGame/src/riskModels/country/CountryConstants.java
+++ RiskGame/src/riskModels/country/CountryConstants.java
@@ -1,5 +1,14 @@
 package riskModels.country;
-
+/**
+ * This class holds constants used by Country Model methods
+ * @author prashantp95
+ *
+ */
 public class CountryConstants {
+	
+	public static final String comma=",";
+	public static final String bracket="[";
+	
+	
 
 }
