7fe80e6af438d99cff174e127a4c2811fb06c365
Akshay Shah <akshay.shah@knappily.com>
2017-10-08 22:28:18 -0400

    [Update][Fix] Coding Convention style set up

--- RiskGame/src/riskModels/continent/Continent.java
+++ RiskGame/src/riskModels/continent/Continent.java
@@ -1,41 +1,43 @@
 package riskModels.continent;
 
+import riskModels.country.Country;
+
 import java.util.ArrayList;
 import java.util.List;
 
-import riskModels.country.Country;
 /**
  * Continent Bean class that will hold the properties of continent.
- * @author prashantp95
  *
+ * @author prashantp95
  */
 public class Continent {
-	
-	public String continentName;
-	public List<Country> memberCountriesList = new ArrayList<Country>();
-	public int numberOfTerritories;
-	
-	public String getContinentName() 
-	{
-		return continentName;
-	}
-	public void setContinentName(String continentName) 
-	{
-		this.continentName = continentName;
-	}
-	public List<Country> getMemberCountriesList()
-	{
-		return memberCountriesList;
-	}
-	public void setMemberCountriesList(List<Country> memberCountriesList)
-	{
-		this.memberCountriesList = memberCountriesList;
-	}
-	public int getNumberOfTerritories() {
-		return numberOfTerritories;
-	}
-	public void setNumberOfTerritories(int nummberOfTerritories) {
-		this.numberOfTerritories = nummberOfTerritories;
-	}
-	
+
+    public String continentName;
+    public List<Country> memberCountriesList = new ArrayList<Country>();
+    public int numberOfTerritories;
+
+    public String getContinentName() {
+        return continentName;
+    }
+
+    public void setContinentName(String continentName) {
+        this.continentName = continentName;
+    }
+
+    public List<Country> getMemberCountriesList() {
+        return memberCountriesList;
+    }
+
+    public void setMemberCountriesList(List<Country> memberCountriesList) {
+        this.memberCountriesList = memberCountriesList;
+    }
+
+    public int getNumberOfTerritories() {
+        return numberOfTerritories;
+    }
+
+    public void setNumberOfTerritories(int nummberOfTerritories) {
+        this.numberOfTerritories = nummberOfTerritories;
+    }
+
 }
--- RiskGame/src/util/RiskGameUtil.java
+++ RiskGame/src/util/RiskGameUtil.java
@@ -1,20 +1,23 @@
 package util;
+
 /**
- * This class will have common utility methods 
+ * This class will have common utility methods
  * for example  checking null string or blank string
- * @author prashantp95
  *
+ * @author prashantp95
  */
 public class RiskGameUtil {
-	/**
-	 * to check null or empty string
-	 * input String , output boolean(true/false)
-	 * @param  name: String to be check.
-	 * @return true or false based on validation
-	 */
-	public static boolean checkNullString(String name) {
-		if(name!="" && name!=null && !name.isEmpty()) {
-			return true;
-		}return false;
-	}
+    /**
+     * to check null or empty string
+     * input String , output boolean(true/false)
+     *
+     * @param name: String to be check.
+     * @return true or false based on validation
+     */
+    public static boolean checkNullString(String name) {
+        if (name != "" && name != null && !name.isEmpty()) {
+            return true;
+        }
+        return false;
+    }
 }
