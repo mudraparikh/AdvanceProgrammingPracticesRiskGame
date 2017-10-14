e2a12134fcc2a675e38b677195f5627ff7eeb55e
mudraparikh <mudraparikh19@gmail.com>
2017-10-14 17:17:42 -0400

    [Updated] LaunchGame selection process

--- RiskGame/src/riskView/map/LaunchGame.java
+++ RiskGame/src/riskView/map/LaunchGame.java
@@ -1,9 +1,10 @@
 package riskView.map;
 
+import javax.swing.*;
+
 import riskModels.map.GameMap;
 import riskModels.map.MapModel;
 
-import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -12,31 +13,51 @@ import java.io.File;
 
 @SuppressWarnings("serial")
 public class LaunchGame extends JPanel {
-    private JLabel label = new JLabel("Enter No of Players (2-4): ");
+    private JLabel label = new JLabel("Select number of Players :");
     private JLabel label1 = new JLabel("Please Select a Correct File");
-    private JLabel label2 = new JLabel("Correct File");
     private JTextField textField = new JTextField(20);
     private JButton button = new JButton("OK");
     private JFileChooser filechooser = new JFileChooser();
     private JDialog dialog = new JDialog();
-
+    private JRadioButton option1 = new JRadioButton("2");
+    private JRadioButton option2 = new JRadioButton("3");
+    private JRadioButton option3 = new JRadioButton("4");
     public LaunchGame() {
     	JFrame frame = new JFrame();
     	frame.setLayout(new BorderLayout());
     	frame.setSize(400,100);
     	frame.setVisible(true);
     	label.setVisible(true);
-    	label1.setVisible(true);
-    	label2.setVisible(true);
         textField.setVisible(true);
         button.setVisible(true);
-        frame.add(label,BorderLayout.LINE_START);
-        frame.add(textField, BorderLayout.LINE_END);
-        frame.add(button, BorderLayout.PAGE_END);
+        ButtonGroup group = new ButtonGroup();
+        group.add(option1);
+        group.add(option2);
+        group.add(option3);
+        
+        frame.add(label,BorderLayout.PAGE_START);
+        frame.add(option1,BorderLayout.LINE_START);
+        frame.add(option2,BorderLayout.CENTER);
+        frame.add(option3,BorderLayout.LINE_END);
+        frame.add(button,BorderLayout.PAGE_END);
+             
         button.addActionListener(new ActionListener() {
-
 			@Override
-			public void actionPerformed(ActionEvent e) {		
+			public void actionPerformed(ActionEvent e) {
+				
+				boolean isoptionSelected;
+		        if(isoptionSelected = option1.isSelected()){
+		        	System.out.println("No. of players is 2");
+		        }
+		        else if(isoptionSelected = option2.isSelected()){
+		        	System.out.println("No. of players is 3");
+		        }
+		        else if(isoptionSelected = option2.isSelected()){
+		        	System.out.println("No. of players is 4");
+		        }
+		        else {
+		        	System.out.println("Select One Player Atleast");
+		        }
 		filechooser.setCurrentDirectory(new File(System.getProperty("user.home")));
         filechooser.setDialogTitle("Select a .map file");
         
@@ -49,9 +70,11 @@ public class LaunchGame extends JPanel {
             if(gameMap.isCorrectMap == false)
             {
             	dialog.setVisible(true);
+            	dialog.setSize(275, 100);
+            	label1.setVisible(true);
+            	label1.setSize(275,100);
             	dialog.setTitle("ERROR");
             	dialog.add(label1);
-            	
             }
             else if(gameMap.isCorrectMap == true)
             {
@@ -59,10 +82,10 @@ public class LaunchGame extends JPanel {
             textField.addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent event) {
-                    dialog.setVisible(true);
-                    dialog.add(label2);
                 	String text = textField.getText();
                     System.out.println(text);
+                    
+                    //open a map
                 }
             });
             }
