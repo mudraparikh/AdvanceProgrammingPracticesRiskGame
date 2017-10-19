0d261e452fa68035ded42ca30a82e1dbdc49cb85
Akshay Shah <akshay.shah@knappily.com>
2017-10-19 04:09:55 -0400

    [New] Add and remove country test cases

--- RiskGame/src/riskModels/map/MapModelTest.java
+++ RiskGame/src/riskModels/map/MapModelTest.java
@@ -3,6 +3,10 @@ package riskModels.map;
 
 import org.junit.Before;
 import org.junit.Test;
+import riskModels.country.Country;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -56,5 +60,31 @@ public class MapModelTest {
     public void testCountryObjectWithNull() throws Exception {
         gameMapObj = mapObj.readMapFile(filePath+ "validate.map");
         assertEquals(null,mapObj.getCountryObj("APP",gameMapObj));
+    }
+
+    @Test
+    public void removeCountry() throws Exception {
+        gameMapObj = mapObj.readMapFile(filePath+ "validate.map");
+        Country countryToRemove = mapObj.getCountryObj("44",gameMapObj);
+        mapObj.removeCountry(countryToRemove,gameMapObj);
+        assertEquals(null,mapObj.getCountryObj("44",gameMapObj));
+    }
+
+    @Test
+    public void addCountry() throws Exception {
+        gameMapObj = mapObj.readMapFile(filePath+ "validate.map");
+        Country country = new Country("100", 23, 34, "Yukon");
+        Country country1 = new Country("101", 43, 94, "Yukon");
+        Country country2 = new Country("102", 63, 23, "Yukon");
+        List<Country> neighboringCountries = new ArrayList<Country>();
+        neighboringCountries.add(country1);
+        neighboringCountries.add(country2);
+        mapObj.addCountry(country,gameMapObj,neighboringCountries);
+        assertEquals("100",mapObj.getCountryObj("100",gameMapObj).getCountryName());
+
+    }
+
+    @Test
+    public void addNeighbor() throws Exception {
     }
 }
