91cef109559bf610b5ffe31cbb41f6c889567b7f
mudraparikh <mudraparikh19@gmail.com>
2017-10-27 17:53:18 -0400

    [Update] new components added

--- RiskGame/src/riskView/GameView.java
+++ RiskGame/src/riskView/GameView.java
@@ -8,10 +8,10 @@ import java.util.Map;
 import javax.swing.*;
 import javax.swing.text.DefaultCaret;
 
-import riskControllers.FileSelectDialogController;
-import riskModels.country.Country;
-import riskModels.map.GameMap;
-import riskModels.map.MapModel;
+import riskControllers.*;
+import riskModels.country.*;
+import riskModels.map.*;
+import riskModels.gamedriver.*;
 
 
 /**
@@ -36,7 +36,9 @@ public class GameView extends JDialog{
 	private JLabel selectedLabel;
 	private JLabel targetLabel;
 	private JLabel continentLabel;
-	
+	private JLabel dominationLabel;
+	private JLabel phaseViewLabel;
+	private JLabel playerViewLabel;
 	
 	private JButton menuBtn;
 	private JButton turnInBtn;
@@ -58,24 +60,31 @@ public class GameView extends JDialog{
 	private DefaultListModel<String> countryDisplay1;
 	private JList countryList2;
 	private DefaultListModel<String> countryDisplay2;
+	
+	private JScrollPane continentScrollPane;
+	private JScrollPane countryScrollPane1;
+	private JScrollPane countryScrollPane2;
+	private JScrollPane phaseViewPane;
+	private JScrollPane playerViewPane;
 
 	private GameMap gameMap;
 	private MapModel mapModel;
+	
+	private JList cardsList;
+	private ListModel cardsListModel;
 	
 	private ImageIcon mapImageIcon;
 	private File mapFile;
 	private JTextArea printTextArea;
 	private DefaultCaret caret;
-	private JScrollPane continentScrollPane;
-	private JScrollPane countryScrollPane1;
-	private JScrollPane countryScrollPane2;
+	
 
 	/**
 	 * Constructs the Risk game board.
 	 **/
 	public GameView() {		
 		
-		setTitle("Java-Risk");
+		setTitle("Risk Game");
 		setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
 		setResizable(false);
 		
@@ -132,7 +141,7 @@ public class GameView extends JDialog{
 	private JPanel messagePanel() {
 	
 		messagePanel = new JPanel();
-		messagePanel.setPreferredSize(new Dimension(300,675));
+		messagePanel.setPreferredSize(new Dimension(290,690));
 		messageLayout = new GridBagLayout();
 		messagePanel.setLayout(messageLayout);
 		
@@ -148,6 +157,14 @@ public class GameView extends JDialog{
 		caret.setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
 		messageScrollPane = new JScrollPane(printTextArea);
 		
+		turnInBtn = new JButton("Turn In Cards");
+		menuBtn = new JButton("Menu");
+		dominationLabel = new JLabel("Player Domaination %:");
+		
+		cardsList = new JList();
+		cardsList.setLayoutOrientation(JList.VERTICAL_WRAP);
+		cardsList.setVisibleRowCount(6);
+		
 		c.fill = GridBagConstraints.BOTH;
 		c.insets = new Insets(5, 5, 5, 5);
 		c.weightx = 0.5;
@@ -155,6 +172,38 @@ public class GameView extends JDialog{
 		c.gridx = 0;
 		c.gridy = 0;
 		messagePanel.add(messageScrollPane, c);
+		
+		c.fill = GridBagConstraints.BOTH;
+		c.insets = new Insets(5, 5, 5, 5);
+		c.weightx = 0.5;
+		c.weighty = 5;
+		c.gridx = 0;
+		c.gridy = 1;
+		messagePanel.add(cardsList, c);
+		
+		c.fill = GridBagConstraints.BOTH;
+		c.insets = new Insets(5, 5, 5, 5);
+		c.weightx = 0.5;
+		c.weighty = 0.5;
+		c.gridx = 0;
+		c.gridy = 2;
+		messagePanel.add(turnInBtn, c);
+		
+		c.fill = GridBagConstraints.BOTH;
+		c.insets = new Insets(5, 5, 5, 5);
+		c.weightx = 0.5;
+		c.weighty = 0.5;
+		c.gridx = 0;
+		c.gridy = 3;
+		messagePanel.add(menuBtn, c);
+		
+		c.fill = GridBagConstraints.BOTH;
+		c.insets = new Insets(5, 5, 5, 5);
+		c.weightx = 0.5;
+		c.weighty = 0.5;
+		c.gridx = 0;
+		c.gridy = 4;
+		messagePanel.add(dominationLabel, c);
 		
 		return messagePanel;
 	}
@@ -166,7 +215,7 @@ private JPanel mapPanel() {
 	mapPanel.setLayout(new GridLayout(1, 1, 5, 5));
 	mapImageIcon = new ImageIcon("Map.jpg");
 	mapScrollPane = new JScrollPane(new JLabel(mapImageIcon));
-	mapScrollPane.setPreferredSize(new Dimension(675, 675));
+	mapScrollPane.setPreferredSize(new Dimension(675, 690));
 	mapPanel.add(mapScrollPane);
 	return mapPanel;
 }
@@ -176,7 +225,7 @@ private JPanel mapPanel() {
 private JPanel actionPanel() {
 	
 	actionPanel = new JPanel();
-	actionPanel.setPreferredSize(new Dimension(175, 675));
+	actionPanel.setPreferredSize(new Dimension(150, 690));
 	actionLayout = new GridBagLayout();
 	actionPanel.setLayout(actionLayout);
 	
@@ -184,9 +233,7 @@ private JPanel actionPanel() {
 	targetLabel = new JLabel("Adjacent Territory:");
 	continentLabel = new JLabel("Continents:");
 	
-	menuBtn = new JButton("Menu");
-	turnInBtn = new JButton("Turn In Cards");
-	reinforceBtn = new JButton("Place Reinforcements");
+	reinforceBtn = new JButton("Reinforce Armies");
 	attackBtn = new JButton("Attack!");
 	fortifyBtn = new JButton("Fortify");
 	endTurnBtn = new JButton("End Turn");
@@ -198,23 +245,33 @@ private JPanel actionPanel() {
 	fortifyBtn.setActionCommand(fortifyBtnName);
 	endTurnBtn.setActionCommand(endTurnBtnName);
 	
+	phaseViewLabel = new JLabel("Phase View:");
+	//model.addObserver(phaseViewLabel);
+	phaseViewPane = new JScrollPane(phaseViewLabel);
+	actionPanel.add(phaseViewPane);
+	
+	playerViewLabel = new JLabel("Player View:");
+	//model.addObserver(playerViewLabel);
+	playerViewPane = new JScrollPane(playerViewLabel);
+	actionPanel.add(playerViewPane);
+	
 	c = new GridBagConstraints();
 
 	c.fill = GridBagConstraints.BOTH;
 	c.insets = new Insets(5, 5, 5, 5);
 	c.weightx = 0.5;
-	c.weighty = 0.5;
+	c.weighty = 5;
 	c.gridx = 0;
 	c.gridy = 0;
-	actionPanel.add(reinforceBtn, c);
+	actionPanel.add(phaseViewPane, c);
 	
 	c.fill = GridBagConstraints.BOTH;
 	c.insets = new Insets(5, 5, 5, 5);
 	c.weightx = 0.5;
-	c.weighty = 0.5;
+	c.weighty = 5;
 	c.gridx = 0;
 	c.gridy = 1;
-	actionPanel.add(attackBtn, c);
+	actionPanel.add(playerViewPane, c);
 	
 	c.fill = GridBagConstraints.BOTH;
 	c.insets = new Insets(5, 5, 5, 5);
@@ -222,7 +279,7 @@ private JPanel actionPanel() {
 	c.weighty = 0.5;
 	c.gridx = 0;
 	c.gridy = 2;
-	actionPanel.add(fortifyBtn, c);
+	actionPanel.add(reinforceBtn, c);
 	
 	c.fill = GridBagConstraints.BOTH;
 	c.insets = new Insets(5, 5, 5, 5);
@@ -230,8 +287,25 @@ private JPanel actionPanel() {
 	c.weighty = 0.5;
 	c.gridx = 0;
 	c.gridy = 3;
+	actionPanel.add(attackBtn, c);
+	
+	c.fill = GridBagConstraints.BOTH;
+	c.insets = new Insets(5, 5, 5, 5);
+	c.weightx = 0.5;
+	c.weighty = 0.5;
+	c.gridx = 0;
+	c.gridy = 4;
+	actionPanel.add(fortifyBtn, c);
+	
+	c.fill = GridBagConstraints.BOTH;
+	c.insets = new Insets(5, 5, 5, 5);
+	c.weightx = 0.5;
+	c.weighty = 0.5;
+	c.gridx = 0;
+	c.gridy = 5;
 	actionPanel.add(endTurnBtn, c);
 	
+	//add two more components observable for phase view
 	return actionPanel;
 }
 /**
@@ -239,32 +313,22 @@ private JPanel actionPanel() {
 **/
 private JPanel countryInfoPanel() {
 	countryInfoPanel = new JPanel();
-	countryInfoPanel.setPreferredSize(new Dimension(350, 675));
+	countryInfoPanel.setPreferredSize(new Dimension(240, 690));
 	actionLayout = new GridBagLayout();
 	countryInfoPanel.setLayout(actionLayout);
 	
-	selectedLabel = new JLabel("Selected Territory:");
-	targetLabel = new JLabel("Adjacent Territory:");
+	selectedLabel = new JLabel("Selected Country:");
+	targetLabel = new JLabel("Neighbouring Countries:");
 	continentLabel = new JLabel("Continents:");
 	
 	gameMap = GameMap.getInstance();
 	mapModel = new MapModel();
 	
-	continentDisplay = new DefaultListModel<>();
-	continentList = new JList(continentDisplay);
-	continentList.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
-	continentList.setLayoutOrientation(JList.VERTICAL_WRAP);
-	continentList.setVisibleRowCount(10);
-	continentScrollPane = new JScrollPane(continentList);
-	continentList.setVisible(true);
-	for (int i = 0; i < gameMap.getContinentList().size(); i++)
-      continentDisplay.addElement(gameMap.getContinentList().get(i).getContinentName());
-	
 	countryDisplay1 = new DefaultListModel<>();
 	countryList1 = new JList(countryDisplay1);
 	countryList1.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
 	countryList1.setLayoutOrientation(JList.VERTICAL_WRAP);
-	countryList1.setVisibleRowCount(10);
+	countryList1.setVisibleRowCount(30);
 	countryScrollPane1 = new JScrollPane(countryList1);
 	countryList1.setVisible(true);
 	for (Map.Entry<Country, List<Country>> e : gameMap.getCountryAndNeighborsMap().entrySet())
@@ -274,11 +338,21 @@ private JPanel countryInfoPanel() {
 	countryList2 = new JList(countryDisplay1);
 	countryList2.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
 	countryList2.setLayoutOrientation(JList.VERTICAL_WRAP);
-	countryList2.setVisibleRowCount(10);
+	countryList2.setVisibleRowCount(30);
 	countryScrollPane2 = new JScrollPane(countryList2);
 	countryList2.setVisible(true);
 	for (Map.Entry<Country, List<Country>> e : gameMap.getCountryAndNeighborsMap().entrySet())
 		countryDisplay2.addElement(e.getKey().getCountryName());
+	
+	continentDisplay = new DefaultListModel<>();
+	continentList = new JList(continentDisplay);
+	continentList.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
+	continentList.setLayoutOrientation(JList.VERTICAL_WRAP);
+	continentList.setVisibleRowCount(50);
+	continentScrollPane = new JScrollPane(continentList);
+	continentList.setVisible(true);
+	for (int i = 0; i < gameMap.getContinentList().size(); i++)
+      continentDisplay.addElement(gameMap.getContinentList().get(i).getContinentName());
 	
 	c = new GridBagConstraints();
 	
@@ -293,7 +367,7 @@ private JPanel countryInfoPanel() {
 	c.fill = GridBagConstraints.BOTH;
 	c.insets = new Insets(5, 5, 5, 5);
 	c.weightx = 0.5;
-	c.weighty = 0.5;
+	c.weighty = 5;
 	c.gridx = 0;
 	c.gridy = 1;
 	countryInfoPanel.add(countryScrollPane1, c);
@@ -309,7 +383,7 @@ private JPanel countryInfoPanel() {
 	c.fill = GridBagConstraints.BOTH;
 	c.insets = new Insets(5, 5, 5, 5);
 	c.weightx = 0.5;
-	c.weighty = 0.5;
+	c.weighty = 5;
 	c.gridx = 0;
 	c.gridy = 3;
 	countryInfoPanel.add(countryScrollPane2, c);
@@ -325,7 +399,7 @@ private JPanel countryInfoPanel() {
 	c.fill = GridBagConstraints.BOTH;
 	c.insets = new Insets(5, 5, 5, 5);
 	c.weightx = 0.5;
-	c.weighty = 0.5;
+	c.weighty = 8;
 	c.gridx = 0;
 	c.gridy = 6;
 	countryInfoPanel.add(continentScrollPane, c);
