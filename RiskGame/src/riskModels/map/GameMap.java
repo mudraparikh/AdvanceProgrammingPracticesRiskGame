6f2af79b20eabc2c9508dde8ec2d71e6bf466a60
Hanita <hnath47@gmail.com>
2017-11-02 16:15:24 -0400

    [Update] JavaDoc

--- RiskGame/src/riskModels/map/GameMap.java
+++ RiskGame/src/riskModels/map/GameMap.java
@@ -33,22 +33,42 @@ public class GameMap {
     	}
     	return gameMap;
     }
+    
+    /**
+     * Default Constructor
+     */
     private GameMap() {
 		//To Prevent Other classes from creating object.
 	}
 
+    /**
+     * this method verify weather the selected map validate the rules of CorrectMap constraints
+     * @return true if the map is correct; otherwise return false
+     */
     public boolean isCorrectMap() {
         return isCorrectMap;
     }
 
+    /** 
+     * setter method checks is the map is correct and then assigns map 
+     * @param isCorrectMap
+     */
     public void setCorrectMap(boolean isCorrectMap) {
         this.isCorrectMap = isCorrectMap;
     }
 
+    /**
+     * getter method for players who selected the map to play on
+     * @return errorMessage message display on the console
+     */
     public String getErrorMessage() {
         return errorMessage;
     }
 
+    /**
+     * setter method to assign the message when we select a map which doesn't agree to the rules
+     * @param errorMessage generates a error message
+     */
     public void setErrorMessage(String errorMessage) {
         this.errorMessage = errorMessage;
 
@@ -62,6 +82,10 @@ public class GameMap {
         return mapDetail;
     }
 
+    /**
+     * setter method to assign mapDetails
+     * @param mapDetail map object
+     */
     public void setMapDetail(HashMap<String, String> mapDetail) {
         this.mapDetail = mapDetail;
 
@@ -75,6 +99,10 @@ public class GameMap {
         return countryAndNeighborsMap;
     }
 
+    /**
+     * setter method to assign countries and its neighbors in the map
+     * @param countryAndNeighborsMap map object
+     */
     public void setCountryAndNeighborsMap(HashMap<Country, List<Country>> countryAndNeighborsMap) {
         this.countryAndNeighborsMap = countryAndNeighborsMap;
     }
@@ -87,18 +115,42 @@ public class GameMap {
         return continentList;
     }
 
+    /**
+     * setter method to assign continents to the map
+     * @param continentList map object
+     */
     public void setContinentList(List<Continent> continentList) {
         this.continentList = continentList;
     }
+    
+    /**
+     * getter method to get list of countries belong to the continent 
+     * @return continentCountryMap map object
+     */
 	public HashMap<Continent, List<Country>> getContinentCountryMap() {
 		return continentCountryMap;
 	}
+	
+	/**
+	 * setter method to assign countries to the continent
+	 * @param continentCountryMap map object
+	 */
 	public void setContinentCountryMap(HashMap<Continent, List<Country>> continentCountryMap) {
 		this.continentCountryMap = continentCountryMap;
 	}
+	
+	/**
+	 * getter method to get the list of the players
+	 * @return playerList player details
+	 */
 	public List<Player> getPlayerList() {
 		return playerList;
 	}
+	
+	/**
+	 * setter method to assign players list
+	 * @param playerList details of players
+	 */
 	public void setPlayerList(List<Player> playerList) {
 		this.playerList = playerList;
 	}
