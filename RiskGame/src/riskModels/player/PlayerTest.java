1e95eddb0dc93b721db2a43655793728e445f2a7
Akshay Shah <akshay.shah@knappily.com>
2017-11-08 21:32:52 -0500

    [New] Player Test file

---
+++ RiskGame/src/riskModels/player/PlayerTest.java
@@ -0,0 +1,52 @@
+package riskModels.player;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PlayerTest {
+    @Test
+    public void initializePlayerData() throws Exception {
+    }
+
+    @Test
+    public void reinforce() throws Exception {
+    }
+
+    @Test
+    public void attack() throws Exception {
+    }
+
+    @Test
+    public void fortify() throws Exception {
+    }
+
+    @Test
+    public void turnInCards() throws Exception {
+    }
+
+    @Test
+    public void endPlayerTurn() throws Exception {
+    }
+
+    @Test
+    public void moveArmyFromTo() throws Exception {
+    }
+
+    @Test
+    public void getMaxNumberOfDicesForDefender() throws Exception {
+    }
+
+    @Test
+    public void getMaxNumberOfDices() throws Exception {
+    }
+
+    @Test
+    public void nextPlayerTurn() throws Exception {
+    }
+
+    @Test
+    public void startGame() throws Exception {
+    }
+
+}
